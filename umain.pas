cc3309e6be2dcbf2a1ab38bf7ef9e831e3af01db
andrejsilva412 <andrej.silva412@gmial.com>
2022-12-14 18:31:58 -0300

    Teste de mensagem

--- umain.pas
+++ umain.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls,
-  JSONPropStorage, ExtCtrls, tgsendertypes;
+  TypInfo, JSONPropStorage, ExtCtrls, tgsendertypes, tgtypes;
 
 type
 
@@ -23,27 +23,30 @@ type
     JSONPropStorage1: TJSONPropStorage;
     Label1: TLabel;
     Label2: TLabel;
+    Memo1: TMemo;
+    Memo2: TMemo;
     PageControl1: TPageControl;
+    Splitter1: TSplitter;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
     tmrIntervalo: TTimer;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure tmrIntervaloTimer(Sender: TObject);
   private
     FBot: TTelegramSender;
-    function TimerIntervaloToString(aIntervalo: TIntervalo): String;
+    procedure BotReceiveUpdate(ASender: TObject; AnUpdate: TTelegramUpdateObj);
+    procedure BotStartCmd(ASender: TObject; const ACommand: String;
+      AMessage: TTelegramMessageObj);
     function TimerIntervaloToInteger(aIntervalo: TIntervalo): Integer;
+    procedure BotSendMessage(ASender: TObject; const ACommand: String;
+      AMessage: TTelegramMessageObj);
+    procedure Notifica;
+    procedure UpdateDelayed(Data: PtrInt);
   public
 
   end;
-
-const
-  C_INTERVALO_5min   =     5000;
-  C_INTERVALO_10min  =    10000;
-  C_INTERVALO_30min  =    60000;
-  C_INTERVALO_1hora  =  1800000;
-  C_INTERVALO_2hora  =  3600000;
-  C_INTERVALO_5horas =  9000000;
 
 var
   frmMain: TfrmMain;
@@ -55,34 +58,115 @@ implementation
 { TfrmMain }
 
 procedure TfrmMain.FormCreate(Sender: TObject);
+var
+  Intervalo: TIntervalo;
 begin
   edToken.Clear;
   JSONPropStorage1.JSONFileName := 'settings.json';
   JSONPropStorage1.Active := true;
   cboIntervalo.Items.Clear;
+  for Intervalo := Low(TIntervalo) to High(TIntervalo) do
+    cboIntervalo.Items.Add(
+      GetEnumName(TypeInfo(TIntervalo), Integer(Intervalo)));
+  Memo1.Clear;
+  Memo2.Clear;
+
+  tmrIntervalo.Interval := 2000;
+ // tmrIntervalo.Enabled := true;
+
+   FBot := TTelegramSender.Create('5964318731:AAGRx8n5EoLSgZSVqL7NodX5xwvXzGWyVYI');
+  FBot.CommandHandlers['/start'] := @BotStartCmd;
+  FBot.CommandHandlers['/sinc'] := @BotSendMessage;
+  FBot.OnReceiveUpdate   := @BotReceiveUpdate;
+  if (Application.Flags*[AppDoNotCallAsyncQueue]) = [] then
+    Application.QueueAsyncCall(@UpdateDelayed, 0);
+
+
 end;
 
-function TfrmMain.TimerIntervaloToString(aIntervalo: TIntervalo): String;
+procedure TfrmMain.FormShow(Sender: TObject);
+var
+  Token: String;
 begin
+  Token := edToken.Text;
+
+  if Token = '' then
+  begin
+    ShowMessage('Token não informado.');
+    exit;
+  end;
 
 
+
+end;
+
+procedure TfrmMain.tmrIntervaloTimer(Sender: TObject);
+begin
+  try
+    tmrIntervalo.Enabled := false;
+    Notifica;
+    tmrIntervalo.Enabled := true;
+  except
+    tmrIntervalo.Enabled := true;
+  end;
 end;
 
 function TfrmMain.TimerIntervaloToInteger(aIntervalo: TIntervalo): Integer;
 begin
 
   case aIntervalo of
-
+    int5min: Result  :=      5000;
+    int10min: Result :=     10000;
+    int30min: Result :=     60000;
+    int1Hora: Result :=   1800000;
+    int2Hora: Result :=   3600000;
+    int5Hora: Result :=   9000000;
   end;
 
+end;
 
-  C_INTERVALO_5min   =     5000;
-  C_INTERVALO_10min  =    10000;
-  C_INTERVALO_30min  =    60000;
-  C_INTERVALO_1hora  =  1800000;
-  C_INTERVALO_2hora  =  3600000;
-  C_INTERVALO_5horas =  9000000;
+procedure TfrmMain.BotSendMessage(ASender: TObject; const ACommand: String;
+  AMessage: TTelegramMessageObj);
+begin
+  Memo2.Lines.Add(ACommand + ' FROM ' + AMessage.From.First_name);
+end;
 
+procedure TfrmMain.BotReceiveUpdate(ASender: TObject;
+  AnUpdate: TTelegramUpdateObj);
+begin
+  if Assigned(AnUpdate) then
+    Memo1.Lines.Add(AnUpdate.Message.AsString);
+end;
+
+procedure TfrmMain.BotStartCmd(ASender: TObject; const ACommand: String;
+  AMessage: TTelegramMessageObj);
+begin
+  Memo2.Lines.Add(ACommand + ' FROM ' + AMessage.From.First_name);
+end;
+
+procedure TfrmMain.Notifica;
+var
+  Token: String;
+begin
+                {
+  FBot := TTelegramSender.Create(Token);
+  try
+
+    if (Application.Flags*[AppDoNotCallAsyncQueue]) = [] then
+      Application.QueueAsyncCall(@UpdateDelayed, 0);
+
+  finally
+    FreeAndNil(FBot);
+  end;
+
+               }
+end;
+
+procedure TfrmMain.UpdateDelayed(Data: PtrInt);
+begin
+  FBot.getUpdates;
+  if (Application.Flags*[AppDoNotCallAsyncQueue]) = [] then
+     Application.QueueAsyncCall(@UpdateDelayed, 0);
 end;
 
 procedure TfrmMain.Button1Click(Sender: TObject);
